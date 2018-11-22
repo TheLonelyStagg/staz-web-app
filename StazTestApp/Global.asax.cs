6d59ed3e9bca9c23efb2355cf7e46b2ef9902b94
TheLonelyStagg <ziom9706@gmail.com>
2018-11-22 23:37:59 +0100

    CurrentCultureSettingFix

--- StazTestApp/Global.asax.cs
+++ StazTestApp/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -9,6 +11,12 @@ namespace StazTestApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pl-PL");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl-PL");
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
